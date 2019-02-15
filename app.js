82d148024212b507f54d2b3d8641df6eb4841df8
Shane Harper <cdtharper@gmail.com>
2019-02-15 10:38:54 -0500

    final prototype

--- app.js
+++ app.js
@@ -157,7 +157,8 @@ var UIController = (function() {
         expensesContainer: '.expenses__list',
         budgetLabel: '.budget__value',
         incomeLabel: '.budget__income--value',
-        
+        expensesLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage'
     }
 
     return {
@@ -205,7 +206,15 @@ var UIController = (function() {
         },
 
         displayBudget: function(obj) {
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+            document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
 
+            if (obj.percentage > 0) {
+                document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';                
+            } else {
+                document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+            }
         },
 
         getDOMstrings: function() {
@@ -244,7 +253,7 @@ var controller = (function(budgetCtrl, UICtrl) {
         var budget = budgetCtrl.getBudget();
 
         // 3. Display the budget on the UI. 
-        console.log(budget);
+        UICtrl.displayBudget(budget);
 
     };
 
@@ -278,6 +287,12 @@ var controller = (function(budgetCtrl, UICtrl) {
     return {
     	init: function() {
     		console.log('app has started');
+            UICtrl.displayBudget({
+                budget: 0,
+                totalInc: 0,
+                totalExp: 0,
+                percentage: -1
+            });
     		setupEventListeners();
     	}
     }
